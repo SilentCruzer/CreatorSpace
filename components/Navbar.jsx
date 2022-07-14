1789934304810aeb523451ebbaceaa80f7f6d886
SilentCruzer <radithyakumar7@gmail.com>
2022-07-14 16:54:57 +0530

    Connects moralis server

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,70 +1,32 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { ConnectButton } from "web3uikit"
-import { Link } from "react-scroll";
+import Link from "next/link"
 
-function Navbar() {
+export default function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<div>
-			<nav className=" shadow-sm fixed w-full z-10">
-				<div className="w-full">
-					<div className="flex items-center h-20 w-full">
-						<div className="flex items-center  mx-20  justify-between w-full">
-							<div className="flex justify-center items-center flex-shrink-0 ">
-								<h1 className=" font-bold text-xl cursor-pointer">
+    return (
+		<div className="pb-2">
+			<nav className="p-6 border-b-2 flex flex-row justify-between items-center">
+			
+            <h1 className=" font-bold text-2xl cursor-pointer">
 									Creator<span className="text-violet-900">Space</span>
 								</h1>
-							</div>
-							<div className="hidden md:block">
-								<div className="ml-10 flex items-baseline space-x-4">
-									<Link
-										activeClass="Home"
-										to="about"
-										smooth={true}
-										offset={50}
-										duration={500}
-										className="cursor-pointer text-violet-900 font-semibold px-3 py-2 text-md hover:font-black"
-									>
-										Home
-									</Link>
-									<Link
-										activeClass="about"
-										to="about"
-										smooth={true}
-										offset={50}
-										duration={500}
-										className="cursor-pointer hover:bg-violet-900 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										About
-									</Link>
-									<Link
-										activeClass="work"
-										to="work"
-										smooth={true}
-										offset={50}
-										duration={500}
-										className="cursor-pointer hover:bg-violet-900 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										Library
-									</Link>
-
-									<Link
-										activeClass="Services"
-										to="work"
-										smooth={true}
-										offset={50}
-										duration={500}
-										className="cursor-pointer hover:bg-violet-900 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-									>
-										Services
-									</Link>
-
-									<ConnectButton moralisAuth={false}/>
-								</div>
-							</div>
-						</div>
-						<div className="mr-10 flex md:hidden ">
+            <div className="flex flex-row items-center invisible md:visible ">
+				<div className="cursor-pointer text-violet-900 font-semibold px-3 py-2 text-md hover:font-black">
+				<Link href="/" >
+                    <a className="mr-1 p-1">Home</a>
+                </Link>	
+				</div>
+                <div className="cursor-pointer hover:bg-purple-900 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+					<Link href="/sell-nft">
+                    <a className="mr-1 p-1">Sell NFT</a>
+                </Link>
+				</div>
+                
+                <ConnectButton moralisAuth={false} />
+            </div>
+			<div className="mr-10 flex md:hidden ">
 							<button
 								onClick={() => setIsOpen(!isOpen)}
 								type="button"
@@ -108,10 +70,9 @@ function Navbar() {
 								)}
 							</button>
 						</div>
-					</div>
-				</div>
-
-				<Transition
+						
+        </nav>
+		<Transition
 					show={isOpen}
 					enter="transition ease-out duration-100 transform"
 					enterFrom="opacity-0 scale-95"
@@ -121,13 +82,14 @@ function Navbar() {
 					leaveTo="opacity-0 scale-95"
 				>
 					{(ref) => (
-						<div className="md:hidden" id="mobile-menu">
-							<div
+						<div className="md:hidden border-b-2 " id="mobile-menu">
+							<ul
 								ref={ref}
 								className="bg-white px-2 pt-2 pb-3 space-y-1 sm:px-3"
 							>
+								<li className="pl-5">
 								<Link
-									href="/home"
+									href="/	"
 									activeClass="home"
 									to="home"
 									smooth={true}
@@ -136,9 +98,11 @@ function Navbar() {
 									className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
 									Home
-								</Link>
-								<Link
-									href="/about"
+								</Link>	
+								</li>
+
+								<li li className="pl-5"><Link
+									href="/sell-nft"
 									activeClass="about"
 									to="about"
 									smooth={true}
@@ -146,53 +110,17 @@ function Navbar() {
 									duration={500}
 									className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 								>
-									About
-								</Link>
-
-								<Link
-									href="/work"
-									activeClass="work"
-									to="work"
-									smooth={true}
-									offset={50}
-									duration={500}
-									className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-								>
-									Projects
-								</Link>
-								<Link
-									href="/services"
-									activeClass="services"
-									to="services"
-									smooth={true}
-									offset={50}
-									duration={500}
-									className="cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-								>
-									Services
-								</Link>
-
-								<ConnectButton moralisAuth={false}/>
-							</div>
+									Sell NFT
+								</Link></li>
+								<li>
+									<ConnectButton moralisAuth={false}/>
+								</li>
+								
+							</ul>
 						</div>
 					)}
 				</Transition>
-			</nav>
 		</div>
-	);
+        
+    )
 }
-
-export default Navbar;
-
-// import { ConnectButton } from "web3uikit"
-
-// export default function Header() {
-//     return (
-//         <nav className="p-5 border-b-2 flex flex-row">
-//             <h1 className="py-4 px-4 font-bold text-3xl"> Creator space</h1>
-//             <div className="ml-auto py-2 px-4">
-//                 <ConnectButton moralisAuth={false}/>
-//             </div>
-//         </nav>
-//     )
-// }
